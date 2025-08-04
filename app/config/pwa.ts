e012fd835fa401c58b43ede673a3a511eaa7687e
Elijah Ellanski <ilya.ellanskiy@itms.tech>
2025-08-04 19:46:24 +0300

    fix: workbox base url

--- app/config/pwa.ts
+++ app/config/pwa.ts
@@ -51,8 +51,8 @@ export const pwa: ModuleOptions = {
   },
   workbox: {
     globPatterns: ['**/*.{js,css,html,txt,png,ico,svg}'],
-    navigateFallbackDenylist: [/^\/api\//],
-    navigateFallback: '/',
+    // navigateFallbackDenylist: [/^\/api\//],
+    navigateFallback: '/learning-profile/',
     cleanupOutdatedCaches: true,
     runtimeCaching: [
       {
